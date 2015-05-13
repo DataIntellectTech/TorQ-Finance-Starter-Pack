2a56f36e0420f44c1184dfe7eb3c55572b1098f3
AndrewShortt <andrew.shortt@aquaq.co.uk>
2015-05-13 12:12:37 +0100

    Added stackid for process identification

--- start_torq_demo.sh
+++ start_torq_demo.sh
@@ -3,7 +3,7 @@
 
 # sets the base port for a default TorQ installation
 export KDBBASEPORT=5000
-
+export KDBSTACKID="-stackid 5000"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KDBLIB/l32
 
 ##### EMAILS #####
@@ -15,48 +15,49 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KDBLIB/l32
 
 # launch the discovery service
 echo 'Starting discovery proc...'
-q torq.q -load code/processes/discovery.q -localtime -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt 1 </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
+q torq.q -load code/processes/discovery.q ${KDBSTACKID}  -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt -localtime  </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
 
 # launch the tickerplant, rdb, hdb
 echo 'Starting tp...'
-q tickerplant.q database hdb -localtime -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt 1 </dev/null >$KDBLOG/torqtp.txt 2>&1 &
+q tickerplant.q database hdb ${KDBSTACKID} -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqtp.txt 2>&1 &
 
 echo 'Starting rdb...'
-q torq.q -load code/processes/rdb.q -localtime -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt 1 -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
+q torq.q -load code/processes/rdb.q ${KDBSTACKID} -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
 
 echo 'Starting hdb1...'
-q torq.q -load hdb/database -localtime -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt 1 -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
+q torq.q -load hdb/database ${KDBSTACKID} -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
+
 echo 'Starting hdb2...'
-q torq.q -load hdb/database -localtime -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt 1 -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
+q torq.q -load hdb/database ${KDBSTACKID} -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
 
 # launch the gateway
 echo 'Starting gw...'
-q torq.q -load code/processes/gateway.q -localtime -proctype gateway -procname gateway1 -U config/passwords/accesslist.txt 1 -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
+q torq.q -load code/processes/gateway.q ${KDBSTACKID} -proctype gateway -procname gateway1 -U config/passwords/accesslist.txt -localtime -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
 
 # launch the monitor
 echo 'Starting monitor...'
-q torq.q -load code/processes/monitor.q -localtime -proctype monitor -procname monitor1 1 </dev/null >$KDBLOG/torqmonitor.txt 2>&1 &
+q torq.q -load code/processes/monitor.q ${KDBSTACKID} -proctype monitor -procname monitor1 -localtime </dev/null >$KDBLOG/torqmonitor.txt 2>&1 &
 
 # launch the reporter
 echo 'Starting reporter...'
-q torq.q -load code/processes/reporter.q -localtime -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt 1 </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
+q torq.q -load code/processes/reporter.q ${KDBSTACKID} -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
 
 # launch housekeeping
 echo 'Starting housekeeping proc...'
-q torq.q -load code/processes/housekeeping.q -localtime -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt 1 </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
+q torq.q -load code/processes/housekeeping.q ${KDBSTACKID} -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
 
 # launch sort processes
 echo 'Starting sorting proc...'
-q torq.q -load code/processes/wdb.q -localtime -proctype sort -procname sort1 -U config/passwords/accesslist.txt 1 -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
+q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype sort -procname sort1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
 
 # launch wdb
 echo 'Starting wdb...'
-q torq.q -load code/processes/wdb.q -localtime -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt 1 -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
+q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
 
 # launch compress
 echo 'Starting compression proc...'
-q torq.q -load code/processes/compression.q -localtime -proctype compression -procname compression1 1 </dev/null >$KDBLOG/torqcompress1.txt 2>&1 &  # compression process
+q torq.q -load code/processes/compression.q ${KDBSTACKID} -proctype compression -procname compression1 -localtime </dev/null >$KDBLOG/torqcompress1.txt 2>&1 &  # compression process
 
 # launch feed
 echo 'Starting feed...'
-q torq.q -load tick/feed.q -localtime -proctype feed -procname feed1 1 </dev/null >$KDBLOG/torqfeed.txt 2>&1 &
+q torq.q -load tick/feed.q ${KDBSTACKID} -proctype feed -procname feed1 -localtime </dev/null >$KDBLOG/torqfeed.txt 2>&1 &
--- start_torq_demo_osx.sh
+++ start_torq_demo_osx.sh
@@ -3,7 +3,7 @@
 
 # sets the base port for a default TorQ Finance Starter Pack installation
 export KDBBASEPORT=5000
-
+export KDBSTACKID="-stackid 5000"
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$KDBLIB/m32 
 
 ##### EMAILS #####
@@ -15,48 +15,48 @@ export DEMOEMAILRECEIVER=test@youremail.com
 
 # launch the discovery service
 echo 'Starting discovery proc...'
-q torq.q -load code/processes/discovery.q -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
+q torq.q -load code/processes/discovery.q ${KDBSTACKID} -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
 
 # launch the tickerplant, rdb, hdb
 echo 'Starting tp...'
-q tickerplant.q database hdb -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqtp.txt 2>&1 &
+q tickerplant.q database hdb ${KDBSTACKID} -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqtp.txt 2>&1 &
 
 echo 'Starting rdb...'
-q torq.q -load code/processes/rdb.q -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
+q torq.q -load code/processes/rdb.q ${STACKID} -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
 
 echo 'Starting hdb1...'
-q torq.q -load hdb/database -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
+q torq.q -load hdb/database ${STACKID} -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
 echo 'Starting hdb2...'
-q torq.q -load hdb/database -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
+q torq.q -load hdb/database ${STACKID} -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
 
 # launch the gateway
 echo 'Starting gw...'
-q torq.q -load code/processes/gateway.q -proctype gateway -procname gateway1 -U config/passwords/accesslist.txt -.servers.CONNECTIONS hdb rdb -localtime -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
+q torq.q -load code/processes/gateway.q ${STACKID} -proctype gateway -procname gateway1 -U config/passwords/accesslist.txt -.servers.CONNECTIONS hdb rdb -localtime -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
 
 # launch the monitor
 echo 'Starting monitor...'
-q torq.q -load code/processes/monitor.q -proctype monitor -procname monitor1 -localtime </dev/null >$KDBLOG/torqmonitor.txt 2>&1 &
+q torq.q -load code/processes/monitor.q ${STACKID} -proctype monitor -procname monitor1 -localtime </dev/null >$KDBLOG/torqmonitor.txt 2>&1 &
 
 # launch the reporter
 echo 'Starting reporter...'
-q torq.q -load code/processes/reporter.q -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
+q torq.q -load code/processes/reporter.q ${STACKID} -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
 
 # launch housekeeping
 echo 'Starting housekeeping proc...'
-q torq.q -load code/processes/housekeeping.q -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
+q torq.q -load code/processes/housekeeping.q ${STACKID} -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
 
 # launch sort processes
 echo 'Starting sorting proc...'
-q torq.q -load code/processes/wdb.q -proctype sort -procname sort1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
+q torq.q -load code/processes/wdb.q ${STACKID} -proctype sort -procname sort1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
 
 # launch wdb
 echo 'Starting wdb...'
-q torq.q -load code/processes/wdb.q -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
+q torq.q -load code/processes/wdb.q ${STACKID} -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
 
 # launch compress
 echo 'Starting compression proc...'
-q torq.q -load code/processes/compression.q -proctype compression -procname compression1 -localtime </dev/null >$KDBLOG/torqcompress1.txt 2>&1 &  # compression process
+q torq.q -load code/processes/compression.q ${STACKID} -proctype compression -procname compression1 -localtime </dev/null >$KDBLOG/torqcompress1.txt 2>&1 &  # compression process
 
 # launch feed
 echo 'Starting feed...'
-q torq.q -load tick/feed.q -proctype feed -procname feed1 -localtime </dev/null >$KDBLOG/torqfeed.txt 2>&1 &
+q torq.q -load tick/feed.q ${STACKID} -proctype feed -procname feed1 -localtime </dev/null >$KDBLOG/torqfeed.txt 2>&1 &
