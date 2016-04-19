9ba5d4f6651c040f12338102965ca6e36dd65389
Andrew Clarke <aclarke@homer.aquaq.co.uk>
2016-04-19 10:38:43 +0100

    Move all contents from config to appconfig. Delete Config. Reference appconfig instead of config in start files.

--- start_torq_demo.bat
+++ start_torq_demo.bat
@@ -21,39 +21,39 @@ REM the address emails will be sent to
 REM DEMOEMAILRECEIVER=test@youremail.com
 
 REM launch the discovery service
-start "discovery" q torq.q -load code/processes/discovery.q -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt -o 0 -localtime 
+start "discovery" q torq.q -load code/processes/discovery.q -proctype discovery -procname discovery1 -U appconfig/passwords/accesslist.txt -o 0 -localtime 
 
 timeout 2
 
 REM launch the tickerplant, rdb, hdb
-start "tickerplant" q code/processes/tickerplant.q database %TORQHOME%/hdb -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt -localtime 
-start "rdb" q torq.q -load code/processes/rdb.q -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt -localtime -g 1
-start "chainedtp" q torq.q -load code/processes/chainedtp.q -proctype chainedtp -procname chainedtp1 -U config/passwords/accesslist.txt -localtime
-start "hdb1" q torq.q -load %KDBHDB% -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt -localtime -g 1 -w 4000
-start "hdb2" q torq.q -load %KDBHDB% -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt -localtime -g 1 -w 4000
+start "tickerplant" q code/processes/tickerplant.q database %TORQHOME%/hdb -proctype tickerplant -procname tickerplant1 -U appconfig/passwords/accesslist.txt -localtime 
+start "rdb" q torq.q -load code/processes/rdb.q -proctype rdb -procname rdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1
+start "chainedtp" q torq.q -load code/processes/chainedtp.q -proctype chainedtp -procname chainedtp1 -U appconfig/passwords/accesslist.txt -localtime
+start "hdb1" q torq.q -load %KDBHDB% -proctype hdb -procname hdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1 -w 4000
+start "hdb2" q torq.q -load %KDBHDB% -proctype hdb -procname hdb2 -U appconfig/passwords/accesslist.txt -localtime -g 1 -w 4000
 
 REM launch the gateway
-start "gateway" q torq.q -load code/processes/gateway.q -proctype gateway -procname gateway1 -.servers.CONNECTIONS hdb rdb -U config/passwords/accesslist.txt -localtime -g 1 -w 4000 
+start "gateway" q torq.q -load code/processes/gateway.q -proctype gateway -procname gateway1 -.servers.CONNECTIONS hdb rdb -U appconfig/passwords/accesslist.txt -localtime -g 1 -w 4000 
 
 REM launch the monitor
 start "monitor" q torq.q -load code/processes/monitor.q -proctype monitor -procname monitor1 -localtime 
 
 REM launch the reporter
-start "reporter" q torq.q -load code/processes/reporter.q -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt -localtime 
+start "reporter" q torq.q -load code/processes/reporter.q -proctype reporter -procname reporter1 -U appconfig/passwords/accesslist.txt -localtime 
 
 REM launch housekeeping
-start "housekeeping" q torq.q -load code/processes/housekeeping.q -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt -localtime 
+start "housekeeping" q torq.q -load code/processes/housekeeping.q -proctype housekeeping -procname housekeeping1 -U appconfig/passwords/accesslist.txt -localtime 
 
 REM launch the wdb and sort processes 
-start "sort" q torq.q -load code/processes/wdb.q -proctype sort -procname sort1 -U config/passwords/accesslist.txt -localtime -g 1
+start "sort" q torq.q -load code/processes/wdb.q -proctype sort -procname sort1 -U appconfig/passwords/accesslist.txt -localtime -g 1
 
-start "wdb" q torq.q -load code/processes/wdb.q -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt -localtime -g 1
+start "wdb" q torq.q -load code/processes/wdb.q -proctype wdb -procname wdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1
 
 REM launch compression
 start "compression" q torq.q -load code/processes/compression.q -proctype compression -procname compression1 -localtime -g 1
 
 REM launch feed
-start "feed" q torq.q -load code/tick/feed.q -proctype feed -procname feed1 -U config/passwords/accesslist.txt -localtime 
+start "feed" q torq.q -load code/tick/feed.q -proctype feed -procname feed1 -U appconfig/passwords/accesslist.txt -localtime 
 
 REM to kill it, run this:
 REM q torq.q -load code/processes/kill.q -proctype kill -procname killtick -.servers.CONNECTIONS rdb wdb tickerplant hdb gateway housekeeping monitor discovery sort reporter compression feed -localtime 
--- start_torq_demo.sh
+++ start_torq_demo.sh
@@ -17,27 +17,27 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KDBLIB/l32
 
 # launch the discovery service
 echo 'Starting discovery proc...'
-q torq.q -load code/processes/discovery.q ${KDBSTACKID} -proctype discovery -procname discovery1 -U config/passwords/accesslist.txt -localtime  </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
+q torq.q -load code/processes/discovery.q ${KDBSTACKID} -proctype discovery -procname discovery1 -U appconfig/passwords/accesslist.txt -localtime  </dev/null >$KDBLOG/torqdiscovery.txt 2>&1 &
 
 # launch the tickerplant, rdb, hdb
 echo 'Starting tp...'
-q code/processes/tickerplant.q database ${TORQHOME}/hdb ${KDBSTACKID} -proctype tickerplant -procname tickerplant1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqtp.txt 2>&1 &
+q code/processes/tickerplant.q database ${TORQHOME}/hdb ${KDBSTACKID} -proctype tickerplant -procname tickerplant1 -U appconfig/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqtp.txt 2>&1 &
 
 echo 'Starting rdb...'
-q torq.q -load code/processes/rdb.q ${KDBSTACKID} -proctype rdb -procname rdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
+q torq.q -load code/processes/rdb.q ${KDBSTACKID} -proctype rdb -procname rdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1 -T 30 </dev/null >$KDBLOG/torqrdb.txt 2>&1 &
 
 echo 'Starting ctp...'
-q torq.q -load code/processes/chainedtp.q ${KDBSTACKID} -proctype chainedtp -procname chainedtp1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqchainedtp.txt 2>&1 &
+q torq.q -load code/processes/chainedtp.q ${KDBSTACKID} -proctype chainedtp -procname chainedtp1 -U appconfig/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqchainedtp.txt 2>&1 &
 
 echo 'Starting hdb1...'
-q torq.q -load ${KDBHDB} ${KDBSTACKID} -proctype hdb -procname hdb1 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
+q torq.q -load ${KDBHDB} ${KDBSTACKID} -proctype hdb -procname hdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb1.txt 2>&1 &
 
 echo 'Starting hdb2...'
-q torq.q -load ${KDBHDB} ${KDBSTACKID} -proctype hdb -procname hdb2 -U config/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
+q torq.q -load ${KDBHDB} ${KDBSTACKID} -proctype hdb -procname hdb2 -U appconfig/passwords/accesslist.txt -localtime -g 1 -T 60 -w 4000 </dev/null >$KDBLOG/torqhdb2.txt 2>&1 &
 
 # launch the gateway
 echo 'Starting gw...'
-q torq.q -load code/processes/gateway.q ${KDBSTACKID} -proctype gateway -procname gateway1 -U config/passwords/accesslist.txt -localtime -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
+q torq.q -load code/processes/gateway.q ${KDBSTACKID} -proctype gateway -procname gateway1 -U appconfig/passwords/accesslist.txt -localtime -g 1 -w 4000 </dev/null >$KDBLOG/torqgw.txt 2>&1 &
 
 # launch the monitor
 echo 'Starting monitor...'
@@ -45,19 +45,19 @@ q torq.q -load code/processes/monitor.q ${KDBSTACKID} -proctype monitor -procnam
 
 # launch the reporter
 echo 'Starting reporter...'
-q torq.q -load code/processes/reporter.q ${KDBSTACKID} -proctype reporter -procname reporter1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
+q torq.q -load code/processes/reporter.q ${KDBSTACKID} -proctype reporter -procname reporter1 -U appconfig/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqreporter.txt 2>&1 &
 
 # launch housekeeping
 echo 'Starting housekeeping proc...'
-q torq.q -load code/processes/housekeeping.q ${KDBSTACKID} -proctype housekeeping -procname housekeeping1 -U config/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
+q torq.q -load code/processes/housekeeping.q ${KDBSTACKID} -proctype housekeeping -procname housekeeping1 -U appconfig/passwords/accesslist.txt -localtime </dev/null >$KDBLOG/torqhousekeeping.txt 2>&1 &
 
 # launch sort processes
 echo 'Starting sorting proc...'
-q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype sort -procname sort1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
+q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype sort -procname sort1 -U appconfig/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqsort.txt 2>&1 & # sort process
 
 # launch wdb
 echo 'Starting wdb...'
-q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype wdb -procname wdb1 -U config/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
+q torq.q -load code/processes/wdb.q ${KDBSTACKID} -proctype wdb -procname wdb1 -U appconfig/passwords/accesslist.txt -localtime -g 1 </dev/null >$KDBLOG/torqwdb.txt 2>&1 &  # pdb process
 
 # launch compress
 echo 'Starting compression proc...'
