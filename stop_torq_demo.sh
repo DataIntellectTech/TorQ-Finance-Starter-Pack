4e466a39ed60c0daa279ff4f5110bb3ce015ef75
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2018-05-17 10:56:51 +0100

    Update stop script with all processes

--- stop_torq_demo.sh
+++ stop_torq_demo.sh
@@ -3,4 +3,4 @@
 
 #kill all torq procs
 echo 'Shutting down TorQ...'
-q torq.q -load code/processes/kill.q -proctype kill -procname killtick -.servers.CONNECTIONS sortslave iexfeed feed rdb tickerplant chainedtp hdb gateway housekeeping monitor discovery wdb sort reporter compression metrics wap checker  </dev/null >$KDBLOG/torqkill.txt 2>&1 &
+q torq.q -load code/processes/kill.q -proctype kill -procname killtick -.servers.CONNECTIONS tickerplant discovery rdb hdb wdb sort gateway monitor housekeeping reporter compression feed chainedtp sortslave metrics iexfeed checker wap summary dqe </dev/null >$KDBLOG/torqkill.txt 2>&1 &
