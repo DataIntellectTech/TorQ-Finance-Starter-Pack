4215c9eedfc1046716291b46fe4af841ebfd993a
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2018-05-23 18:53:33 +0100

    #40 Add example of cron job to bash wrapper

--- eodsummary.sh
+++ eodsummary.sh
@@ -1,3 +1,6 @@
+#This script is designed to be run on cron, such that the eodsummary process can be regularly run, example cronjob below:
+#```*/1 * * * * QLIC=/opt/kdb/QLIC QHOME=/opt/kdb/3.5/2017.11.30 rlwrap -r /opt/kdb/3.5/2017.11.30/l64/q $HOME/devbranch/prodDev/TorQ-Finance-Starter-Pacl/eodsummary.sh```
+
 . ../setenv.sh
 
 iiexport KDBHDB=${PWD}/../hdb/database
