f9b6e254b26e4960b6ee5525c919a528c80e74c1
Aqualex <rdanutalexandru@gmail.com>
2018-08-07 17:02:51 +0100

    Added ${BASEDIR} in process.csv to so that torq can be executed from root. Removed TORQHOME env variable from setenv Replaced ${PWD} with ${TORQHOME}

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,7 @@
 # if running the kdb+tick example, change these to full paths
 # some of the kdb+tick processes will change directory, and these will no longer be valid
-export TORQHOME=${PWD}
+
+#export TORQHOME=${PWD}
 export KDBCONFIG=${TORQHOME}/config
 export KDBCODE=${TORQHOME}/code
 export KDBLOG=${TORQHOME}/logs
@@ -24,10 +25,10 @@ export TORQPROCESSES=${KDBAPPCONFIG}/process.csv
 
 touch $KDBLOG/torqsslcert.txt
 if [ -z "${SSL_CA_CERT_FILE}" ]; then
-	mkdir ${PWD}/certs
-	curl -s  https://curl.haxx.se/ca/cacert.pm > ${PWD}/certs/cabundle.pem
-	echo "`date`    The SSL securiity certificate has been downloaded to ${PWD}/certs/cabundle.pem" </dev/null >>$KDBLOG/torqsslcert.txt 
-	export SSL_CA_CERT_FILE=${PWD}/certs/cabundle.pem
+	mkdir ${TORQHOME}/certs
+	curl -s  https://curl.haxx.se/ca/cacert.pm > ${TORQHOME}/certs/cabundle.pem
+	echo "`date`    The SSL securiity certificate has been downloaded to ${TORQHOME}/certs/cabundle.pem" </dev/null >>$KDBLOG/torqsslcert.txt 
+	export SSL_CA_CERT_FILE=${TORQHOME}/certs/cabundle.pem
 else
 	echo "`date`    The SSL security certificate already exists. If https requests fail it may be because of inappropriate certification." </dev/null >>$KDBLOG/torqsslcert.txt 
 fi
