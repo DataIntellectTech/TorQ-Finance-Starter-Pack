4b793c72452424ce3f45a8a3b2c0615d6c254579
Aqualex <rdanutalexandru@gmail.com>
2018-08-08 15:06:00 +0100

    Added small script to determine the path to direcotry based on current directory Modified TORQHOME env variable to have a dynamically generated path

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,14 @@
 # if running the kdb+tick example, change these to full paths
 # some of the kdb+tick processes will change directory, and these will no longer be valid
 
-#export TORQHOME=${PWD}
+if [ "-bash" = $0 ]; then
+    dirpath="${BASH_SOURCE[0]}"
+else
+    dirpath="$0"
+fi
+
+export TORQHOME=$(dirname $dirpath)
+export BASEDIR=${TORQHOME}
 export KDBCONFIG=${TORQHOME}/config
 export KDBCODE=${TORQHOME}/code
 export KDBLOG=${TORQHOME}/logs
