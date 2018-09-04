ff3917204ea33ef31848831ab29f877a082680d8
Alex <34272934+Aqualex@users.noreply.github.com>
2018-09-04 12:46:31 +0100

    [WIP] Integrating Monit for monitoring TorQ (#29)
    * Added ${BASEDIR} in process.csv to so that torq can be executed from root.
    Removed TORQHOME env variable from setenv
    Replaced ${PWD} with ${TORQHOME}
    
    * Added small script to determine the path to direcotry based on current directory
    Modified TORQHOME env variable to have a dynamically generated path
    
    * Removed BASEDIR varaible
    Replaced BASEDIR with TORQHOME in process.csv

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,13 @@
 # if running the kdb+tick example, change these to full paths
 # some of the kdb+tick processes will change directory, and these will no longer be valid
-export TORQHOME=${PWD}
+
+if [ "-bash" = $0 ]; then
+    dirpath="${BASH_SOURCE[0]}"
+else
+    dirpath="$0"
+fi
+
+export TORQHOME=$(dirname $dirpath)
 export KDBCONFIG=${TORQHOME}/config
 export KDBCODE=${TORQHOME}/code
 export KDBLOG=${TORQHOME}/logs
@@ -24,10 +31,10 @@ export TORQPROCESSES=${KDBAPPCONFIG}/process.csv
 
 touch $KDBLOG/torqsslcert.txt
 if [ -z "${SSL_CA_CERT_FILE}" ]; then
-	mkdir ${PWD}/certs
-	curl -s  https://curl.haxx.se/ca/cacert.pm > ${PWD}/certs/cabundle.pem
-	echo "`date`    The SSL securiity certificate has been downloaded to ${PWD}/certs/cabundle.pem" </dev/null >>$KDBLOG/torqsslcert.txt 
-	export SSL_CA_CERT_FILE=${PWD}/certs/cabundle.pem
+	mkdir ${TORQHOME}/certs
+	curl -s  https://curl.haxx.se/ca/cacert.pm > ${TORQHOME}/certs/cabundle.pem
+	echo "`date`    The SSL securiity certificate has been downloaded to ${TORQHOME}/certs/cabundle.pem" </dev/null >>$KDBLOG/torqsslcert.txt 
+	export SSL_CA_CERT_FILE=${TORQHOME}/certs/cabundle.pem
 else
 	echo "`date`    The SSL security certificate already exists. If https requests fail it may be because of inappropriate certification." </dev/null >>$KDBLOG/torqsslcert.txt 
 fi
