4083f0ad2f653561d641e9622151e0aec10fead4
dmartinaquaq <David.Martin@aquaq.co.uk>
2020-02-04 12:42:25 +0000

    Change current dqe process to dqc

--- setenv.sh
+++ setenv.sh
@@ -18,7 +18,7 @@ export KDBHTML=${TORQHOME}/html
 export KDBLIB=${TORQHOME}/lib
 export KDBHDB=${TORQHOME}/hdb/database
 export KDBWDB=${TORQHOME}/wdbhdb
-export KDBDQEDB=${TORQHOME}/dqedb/database
+export KDBDQCDB=${TORQHOME}/dqe/dqcdb/database
 
 # set rlwrap and qcon paths for use in torq.sh qcon flag functions
 export RLWRAP="rlwrap"
