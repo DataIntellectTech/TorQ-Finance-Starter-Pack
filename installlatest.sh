8775d019e01521c5475682b56267b1104148b10b
SanderRoomus <Sander.Roomus@aquaq.co.uk>
2020-10-02 13:15:29 +0100

    New parameters and new installation script call

--- installlatest.sh
+++ installlatest.sh
@@ -1,19 +1,31 @@
+#!/bin/bash
+
+#example usage:
+#bash installlatest.sh
 
 get_latest_release() {
 	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
 }
 
-
-##Change this to correct path after release
 wget https://raw.githubusercontent.com/AquaQAnalytics/TorQ/master/installtorqapp.sh
 
 torq_latest=`get_latest_release "AquaQAnalytics/TorQ"`
 
-echo "============================================================="
-echo "Latest TorQ release"
-echo $torq_latest
-echo "Getting the latest TorQ .tar.gz file"
-echo "============================================================="
+if [[  $torq_latest == *?.?.? ]] || [[  $torq_latest == *?.??.?? ]];
+then
+        echo "============================================================="
+	echo "Latest TorQ release"
+	echo $torq_latest
+	echo "Getting the latest TorQ .tar.gz file"
+	echo "============================================================="
+
+	
+else
+	echo "the tag for Torq release: "
+        echo $torq_latest
+        echo "Is not in the right format, exiting script."
+	exit 1
+fi
 
 wget --content-disposition https://github.com/AquaQAnalytics/TorQ/archive/$torq_latest.tar.gz
 
@@ -26,10 +38,7 @@ else
   torq_latest=${torq_latest#?}
 fi
 
-
 echo $torq_latest
-
-
 
 torq_fsp_latest=`get_latest_release "AquaQAnalytics/TorQ-Finance-Starter-Pack"`
 
@@ -38,6 +47,23 @@ echo "Latest TorQ-FSP release"
 echo $torq_fsp_latest
 echo "Getting the latest TorQ-FSP .tar.gz file"
 echo "============================================================="
+
+if [[  $torq_fsp_latest == *?.?.? ]] || [[  $torq_fsp_latest == *?.??.?? ]];
+then
+	echo "============================================================="
+	echo "Latest TorQ-FSP release"
+	echo $torq_fsp_latest
+	echo "Getting the latest TorQ-FSP .tar.gz file"
+	echo "============================================================="
+
+	
+else
+        echo "the tag for Torq release: "
+        echo $torq_fsp_latest
+        echo "Is not in the right format, exiting script."
+	exit 1
+fi
+
 
 wget --content-disposition https://github.com/AquaQAnalytics/TorQ-Finance-Starter-Pack/archive/$torq_fsp_latest.tar.gz
 
@@ -50,9 +76,8 @@ else
   torq_fsp_latest=${torq_fsp_latest#?}
 fi
 
-
 echo $torq_fsp_latest
 
-cho "Files downloaded. Executing install script"
+echo "Files downloaded. Executing install script"
 
-sh installtorqapp.sh torq=TorQ-$torq_latest.tar.gz releasedir=deploy data=datatemp installfile=TorQ-Finance-Starter-Pack-$torq_fsp_latest.tar.gz
+bash installtorqapp.sh --torq TorQ-$torq_latest.tar.gz --releasedir deploy --data datatemp --installfile TorQ-Finance-Starter-Pack-$torq_fsp_latest.tar.gz
