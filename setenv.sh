0e221ba73302ba850aabd11d48bce99d73ff087c
Eoghan O'Hara <eoghan.ohara@dataintellect.com>
2026-02-09 16:31:56 +0000

    remove iex references & tables

--- setenv.sh
+++ setenv.sh
@@ -51,9 +51,6 @@ export DOGSTATSD_PORT=8125
 # e.g. macOS:
 # export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$KDBLIB/m[32|64]
 
-# Please input the API token obtained from IEX here
-export IEX_PUBLIC_TOKEN=""
-
 TORQSSLCERT=${KDBLOG}/torqsslcert.txt
 touch ${TORQSSLCERT}
 if [ -z "${SSL_CA_CERT_FILE}" ]; then
